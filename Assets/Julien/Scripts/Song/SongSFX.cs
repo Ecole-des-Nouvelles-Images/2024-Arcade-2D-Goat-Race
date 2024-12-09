0317c46d5d3f6fba676f62bf86808fcf3d54587c
Iluje <juliend1305@gmail.com>
2024-12-09 15:10:55 +0100

    Add soundScript

--- Assets/Julien/Scripts/Song/SongSFX.cs
+++ Assets/Julien/Scripts/Song/SongSFX.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class SongSFX : MonoBehaviour
 {
-    public AudioSource JumpSong;
-    public AudioSource DamageOpstacle;
-    public AudioSource DestroyObject;
+    
+   [Header("<color=yellow>Audio Jumping </color>")]
+    public List<AudioClip> AudioJump;
+    public List<AudioClip> AudioLanding;
+    [Header("--------------------------------------------------")]
+    
+    [Header("<color=red>Audio Damage Obstacle </color>")]
+    public List<AudioClip> AudioDamage;
+    public List<AudioClip> AudioDestroy;
+    [Header("--------------------------------------------------")]
+    
+    [Header("<color=purple>Audio Stun </color>")]
+    public List<AudioClip> AudioStun;
+    [Header("--------------------------------------------------")]
+  
+    [Header("<color=green>Audio Step </color>")]
+    public List<AudioClip> AudioStep;
 }
